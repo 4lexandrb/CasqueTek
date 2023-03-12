5281f84ee36b6309250de474c84701b6df21d28b
Charles Henri Hébert-Michaud <c2hm@outlook.com>
2023-03-11 23:42:37 -0500

    get distance between 2 ble devices

--- Ble/ble_server/src/main.cpp
+++ Ble/ble_server/src/main.cpp
@@ -26,64 +26,107 @@
 // BLE Characteristic
 #define CHARAC1_UUID "c1a1d466-344c-4be3-ab3f-189f80dd7518"
 #define CHARAC2_UUID "c2a1d466-344c-4be3-ab3f-189f80dd7518"
-#define CHARAC3_UUID "c3a1d466-344c-4be3-ab3f-189f80dd7518"
-#define CHARAC4_UUID "c4a1d466-344c-4be3-ab3f-189f80dd7518"
 
 BLECharacteristic charac_1(CHARAC1_UUID, BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
 BLECharacteristic charac_2(CHARAC2_UUID, BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
-BLECharacteristic charac_3(CHARAC3_UUID, BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
-BLECharacteristic charac_4(CHARAC4_UUID, BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
 
 BLEDescriptor descrip_1(BLEUUID((uint16_t)0x2902));
 BLEDescriptor descrip_2(BLEUUID((uint16_t)0x2902));
-BLEDescriptor descrip_3(BLEUUID((uint16_t)0x2902));
-BLEDescriptor descrip_4(BLEUUID((uint16_t)0x2902));
 
 //Variables
 int devicesConnected = 0;
 bool startAdvertize = true;
 BLEServer *pServer;
-	
-class MyServerCallbacks: public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
+uint8_t distance = 0;
+
+class Monitor: public BLEServerCallbacks
+{
+public:
+	static int16_t connection_id;
+
+	/* dBm to distance parameters; How to update distance_factor 1.place the
+	 * phone at a known distance (2m, 3m, 5m, 10m) 2.average about 10 RSSI
+	 * values for each of these distances, Set distance_factor so that the
+	 * calculated distance approaches the actual distances, e.g. at 5m. */
+	static constexpr float reference_power  = -50; //rssi reffrence 
+	static constexpr float distance_factor = 3.5; 
+ 
+	uint8_t get_value() { return value++; }
+	esp_err_t get_rssi() { return esp_ble_gap_read_rssi(remote_addr); }
+ 
+	static float get_distance(const int8_t rssi)
+	{ return pow(10, (reference_power - rssi)/(10*distance_factor)) * 10; }
+ 
+private:
+  void onConnect(BLEServer* pServer, esp_ble_gatts_cb_param_t *param) 
+  {
+    // Update connection variables
+    connection_id = param->connect.conn_id;
+    memcpy(&remote_addr, param->connect.remote_bda, sizeof(remote_addr));
+
+    // Install the RSSI callback
+    BLEDevice::setCustomGapHandler(&Monitor::rssi_event);
+
     devicesConnected++;
     if (devicesConnected < 4)
     {
       startAdvertize = true;
     }
-  };
-  void onDisconnect(BLEServer* pServer) {
+  }
+ 
+  void onDisconnect(BLEServer* pServer)
+  {
+		Serial.printf("Connection #%i closed\n", connection_id);
+		BLEDevice::setCustomGapHandler(nullptr);
+		connection_id = -1;
     devicesConnected--;
+    startAdvertize = true;
     if (devicesConnected < 0)
     {
       devicesConnected = 0;
     }
   }
+ 
+  static void rssi_event(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+ 
+	static esp_bd_addr_t remote_addr;
+	uint8_t value = 0;
 };
+ 
+int16_t Monitor::connection_id = -1;
+esp_bd_addr_t Monitor::remote_addr = {};
+ 
+void Monitor::rssi_event(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
+{
+  static int16_t rssi_average = 0;
+  if (event == ESP_GAP_BLE_READ_RSSI_COMPLETE_EVT)
+  {
+    // Adjust damping_factor to lower values to have a more reactive response
+    const float damping_factor = 0.8;
+    rssi_average = rssi_average * damping_factor + param->read_rssi_cmpl.rssi * (1 - damping_factor);
+    distance = get_distance(rssi_average);
+  }
+}
+ 
+Monitor monitor;
 	
 void setup()
 {
   //Create server
   BLEDevice::init(BLE_SERVER_NAME);
   pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+  pServer->setCallbacks(&monitor);
 
   // Create the BLE Services
   BLEService *service_1 = pServer->createService(SERVICE_UUID);
 
   service_1->addCharacteristic(&charac_1);
   service_1->addCharacteristic(&charac_2);
-  service_1->addCharacteristic(&charac_3);
-  service_1->addCharacteristic(&charac_4);
 
   descrip_1.setValue("charac 1");
   charac_1.addDescriptor(&descrip_1);
   descrip_2.setValue("charac 2");
   charac_2.addDescriptor(&descrip_2);
-  descrip_3.setValue("charac 3");
-  charac_3.addDescriptor(&descrip_3);
-  descrip_4.setValue("charac 4");
-  charac_4.addDescriptor(&descrip_4);
 
   service_1->start();
 
@@ -95,15 +138,14 @@ void setup()
 
 unsigned long time1 = 0;
 unsigned long time2 = 0;
-unsigned long time3 = 0;
-unsigned long time4 = 0;
 static uint8_t mode1 = 0;
-static uint8_t mode2 = 0;
-static uint8_t mode3 = 0;
-static uint8_t mode4 = 0;
+
 
 void loop()
 {
+	static const uint32_t REFRESH_DELAY = 100;
+	static uint32_t next_detection;
+
   if (startAdvertize)
   {
     pServer->getAdvertising()->start();
@@ -119,27 +161,22 @@ void loop()
       charac_1.notify();
       time1 = millis();
     }
-    //charac 2 blinks at 0.5sec
-    if ((millis() - time2) > 2000) {
-      mode2 = !mode2; 
-      charac_2.setValue(&mode2, 1);
-      charac_2.notify();
-      time2 = millis();
-    }
-    //charac 3 blinks at 1sec
-    if ((millis() - time3) > 3000) {
-      mode3 = !mode3; 
-      charac_3.setValue(&mode3, 1);
-      charac_3.notify();
-      time3 = millis();
-    }
-    //charac 4 blinks at 2sec
-    if ((millis() - time4) > 4000) {
-      mode4 = !mode4; 
-      charac_4.setValue(&mode4, 1);
-      charac_4.notify();
-      time4 = millis();
-    }
+    if (millis() - next_detection >= REFRESH_DELAY)
+		{
+			// Prepare for the next detection
+			next_detection += REFRESH_DELAY;
+
+			// Update the internal value (what for?)
+			auto value = monitor.get_value();
+			//Serial.printf("*** NOTIFY: %d ***\n", value);
+			charac_2.setValue(&distance, sizeof(distance));
+			charac_2.notify();
+	 
+			// Request RSSI from the remote address
+			if (monitor.get_rssi() != ESP_OK)
+				Serial.println("RSSI request failed");
+		}
+
   }
 }
 
