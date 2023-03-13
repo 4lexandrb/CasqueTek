60c52512f87a9184db649c93c106d191a21ae94f
4lexandrb <bara1102@usherbrooke.ca>
2023-03-13 08:47:40 -0500

    Final

---
+++ Code/Speaker/Speaker.ino
@@ -0,0 +1,35 @@
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+void setup() {
+
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    int noteDuration = 1000 / noteDurations[thisNote]; // to calculate the note duration, take one second divided by the note type.
+
+    tone(8, melody[thisNote], noteDuration);
+ 
+    int pauseBetweenNotes = noteDuration * 1.30;  // to distinguish the notes, set a minimum time between them.
+
+    delay(pauseBetweenNotes);
+
+    noTone(8); // stop the tone playing:
+
+  }
+}
+
+void loop() {
+
+  // no need to repeat the melody.
+}
